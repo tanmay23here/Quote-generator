cc42c6fdb235fee831e47c9932d6729e99a861b7
Tanmay Kakulte <121185410+tanmay23here@users.noreply.github.com>
2025-05-18 20:07:56 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,7 @@ export default {
         'inner-lg': 'inset 0 2px 15px 0 rgba(0, 0, 0, 0.2)',
       },
       backgroundImage: {
-        'parchment': "url(/src/assets/bg.png)",
+        'parchment': "url('./src/assets/bg.png')",
       },
     },
   },
